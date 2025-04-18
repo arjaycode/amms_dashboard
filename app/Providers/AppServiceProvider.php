69d8462934035bb2ce92f6928552e617c84d9845
ARJAY ARANAS <aranasarjay1@gmail.com>
2025-04-18 23:14:17 +0800

    initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+use LaravelFrontendPresets\MaterialPreset\MaterialPresetServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->register(MaterialPresetServiceProvider::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+    }
+}
